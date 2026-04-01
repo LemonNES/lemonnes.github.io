514a1fe4d4202a8aca4aaf7685b4cf3d24bfc992
LemonNES <nathanvinson09@icloud.com>
2026-03-31 19:05:26 -0500

    Remove Mapper4 as a test

--- nes_core_2026-03-31_190208.js
+++ nes_core_2026-03-31_190208.js
@@ -597,55 +597,6 @@ class Mapper3 extends Mapper {
                 // $E000-$FFFE: IRQ disable
                 // ===== CRITICAL: Disable IRQ and CLEAR the IRQ line =====
                 this.irqEnable = false;
-                if (this.cpu) {
-                    this.cpu.irqLine = false;  // Clear the level-triggered IRQ line
-                }
-            }
-        } else {
-            if (addr < 0xA000) {
-                this.bankData[this.bankSelect & 7] = val;
-                (this.bankSelect & 7) >= 6 ? this.updatePrgMapping() : this.updateChrMapping();
-            } else if (addr < 0xC000) {
-                // $A001-$BFFF: PRG RAM protect (not implemented)
-            } else if (addr < 0xE000) {
-                // $C001-$DFFF: IRQ reload
-                // ===== Set reload flag - counter will reload on NEXT clock =====
-                this.irqCounter = 0;
-                this.irqReloadPending = true;
-            } else {
-                // $E001-$FFFF: IRQ enable
-                // ===== CRITICAL: Enable IRQ =====
-                // Note: Enabling IRQ does NOT immediately assert the line.
-                this.irqEnable = true;
-                // Do NOT assert cpu.irqLine here - it only fires when counter hits 0
-            }
-        }
-    }
-
-    // ===== Update PRG Bank Mapping =====
-    updatePrgMapping() {
-        // PRG mode determines which banks are mapped where
-        const b8 = this.prgMode === 0 ? this.bankData[6] : this.lastBank - 1;
-        const bC = this.prgMode === 0 ? this.lastBank - 1 : this.bankData[6];
-        this.prgBanks[0] = b8;
-        this.prgBanks[1] = this.bankData[7];
-        this.prgBanks[2] = bC;
-        this.prgBanks[3] = this.lastBank;
-    }
-
-    // ===== Update CHR Bank Mapping =====
-    updateChrMapping() {
-        const R = this.bankData;
-        this.chrBanks[0] = R[0] & 0xFE;
-        this.chrBanks[1] = R[0] | 1;
-        this.chrBanks[2] = R[1] & 0xFE;
-        this.chrBanks[3] = R[1] | 1;
-        this.chrBanks[4] = R[2];
-        this.chrBanks[5] = R[3];
-        this.chrBanks[6] = R[4];
-        this.chrBanks[7] = R[5];
-    }
-}
 
   class Mapper9 extends Mapper {
   constructor(cart) {
@@ -798,7 +749,7 @@ class Mapper3 extends Mapper {
 
   // Mapper registry: Index matches iNES mapper ID
   const mappers = [
-    Mapper0, Mapper1, Mapper2, Mapper3, Mapper4,
+    Mapper0, Mapper1, Mapper2, Mapper3, null,
     null, null, null, null, Mapper9 // Index 9 = Mapper9
   ];
 
